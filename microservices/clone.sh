d441bc07faa1fb47f6b3eddaea75222a9eb10e6b
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-09-12 17:01:51 +0200

    :sparkles: Adding task.trustup.io to microservices.

--- microservices/clone.sh
+++ microservices/clone.sh
@@ -2,4 +2,5 @@
 
 # Cloning repositories
 ../_includes/scripts/clone_repos.sh git@github.com:henrotaym/media.trustup.io.git \
-    git@github.com:henrotaym/messaging.trustup.io.git
+    git@github.com:henrotaym/messaging.trustup.io.git \
+    git@github.com:henrotaym/task.trustup.io.git
