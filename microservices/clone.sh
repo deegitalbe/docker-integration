ece80e614366d3f6599bd2a420b03bc278be38af
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2023-01-05 10:31:23 +0100

    :bug: media ownershipt transfered to deegitalbe.

--- microservices/clone.sh
+++ microservices/clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Cloning repositories
-../_includes/scripts/clone_repos.sh git@github.com:henrotaym/media.trustup.io.git \
+../_includes/scripts/clone_repos.sh git@github.com:deegitalbe/trustup-io-media.git \
     git@github.com:henrotaym/messaging.trustup.io.git \
     git@github.com:henrotaym/task.trustup.io.git \
     git@github.com:deegitalbe/websocket-trustup-io.git \
